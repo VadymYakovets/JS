7ce53acc5ba7a23564017fb1456a3f32a6edb59a
VadymYakovets <vad.yakovets@gmail.com>
2019-04-06 17:20:31 +0300

    upload 10 module

--- JS_additional_tasks/ClassWork.23_WebPack/src/js/createDom.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-import '../sass/title.scss'; // импортируем стили
-
-let h1 = document.createElement('h1');
-h1.textContent = 'Hello WebPack';
-
-
-export default h1;
--- JS_additional_tasks/ClassWork.23_WebPack/src/js/img.js
+++
@@ -1,5 +0,0 @@
-import webpackImg from './maxresdefault.jpg';
-let picture = document.createElement('img');
-picture.setAttribute('src', webpackImg);
-
-export default picture;
